1a6bbeba33d8913403a58ca2c23faf1fc694e5d3
nick <nikyasnogorodskyi@gmail.com>
2021-11-09 01:21:21 +0200

    feat: wallpapers

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -60,6 +60,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
