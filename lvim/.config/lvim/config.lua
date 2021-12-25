f3404ed35384be1f36507baeae00d4fdbc450c0d
nick <nikyasnogorodskyi@gmail.com>
2021-12-25 20:18:24 +0200

    feat: setup wifi, fix lvim, add new pacman backup

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -65,7 +65,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
-lvim.builtin.nvimtree.hide_dotfiles = 0
+-- lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
