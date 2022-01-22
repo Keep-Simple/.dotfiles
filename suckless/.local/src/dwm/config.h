cd8460b9aa017b4d6cb7f46ce9241aa04a152a4f
nick <nikyasnogorodskyi@gmail.com>
2022-01-22 12:04:41 +0200

    feat: add lf icons

--- suckless/.local/src/dwm/config.h
+++ suckless/.local/src/dwm/config.h
@@ -99,8 +99,8 @@ static const Layout layouts[] = {
       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 #define STACKKEYS(MOD, ACTION)                                                 \
-  {MOD, XK_j, ACTION##stack, {.i = INC(+1)}},                                  \
-      {MOD, XK_k, ACTION##stack, {.i = INC(-1)}},                              \
+  {MOD, XK_j, ACTION##stack, {.i = INC(-1)}},                                  \
+      {MOD, XK_k, ACTION##stack, {.i = INC(+1)}},                              \
       {MOD,                                                                    \
        XK_v,                                                                   \
        ACTION##stack,                                                          \
