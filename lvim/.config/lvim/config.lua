838ff13f1ca77ac7fd45ae43f4d05df72561e33a
Nikita Yasnogorodskyi <nikita@orca.security>
2022-01-28 16:29:05 +0200

    feat: setup typescript inside Lunarvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,27 +1,13 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
-
--- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = ","
+
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.command_mode["w!!"] = "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!<cr>"
--- map <leader>c :w! \| !compiler "<c-r>%"<CR>
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
@@ -58,10 +44,19 @@ lvim.builtin.which_key.mappings["t"] = {
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
 }
+
+lvim.builtin.which_key.mappings["lt"] = {
+      name = "+Typescript",
+      i = {"<cmd>TSLspImportAll<cr>", "Import All"},
+      o = {"<cmd>TSLspOrganize<cr>", "Organize Imports"},
+      r = {"<cmd>TSLspRenameFile<cr>", "Rename File"},
+      c = {"<cmd>TSLspImportCurrent<cr>", "Import under cursor"},
+      h = {"<cmd>TSLspToggleInlayHints<cr>", "Toggle hints"}
+}
+
 -- lvim.builtin.which_key.mappings[";"] = { "<cmd>ToggleTerm<CR>", "Terminal" }
 -- lvim.builtin.which_key.mappings["d"] = { "<cmd>Dashboard<CR>", "Dashboard" }
 
--- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
@@ -84,76 +79,29 @@ lvim.builtin.treesitter.ensure_installed = {
   "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
--- generic LSP settings
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
-
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
 
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  -- { exe = "black" },
   {
     exe = "prettier",
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
   },
+  -- { exe = "black" },
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  -- { exe = "black" },
   {
     exe = "eslint_d",
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
   },
+  -- { exe = "black" },
 }
 
--- -- set additional linters
--- lvim.lang.typescript.linters = {
---   {exe = "eslint"}
--- }
--- lvim.lang.typescript.formatters = { { exe = "eslint_d"}, { exe = "prettier" } }
--- lvim.lang.typescriptreact.formatters = lvim.lang.typescript.formatters
--- lvim.lang.typescript.linters = { { exe = "eslint" } }
--- lvim.lang.tytreact.linters = lvim.lang.typescript.linters
 
 -- Additional Plugins
 lvim.plugins = {
@@ -205,12 +153,60 @@ lvim.plugins = {
     config = function() require("nvim-ts-autotag").setup() end,
     event = "InsertEnter",
   },
-   --{
-   --  "jose-elias-alvarez/nvim-lsp-ts-utils"
-  -- },
+  {
+    "jose-elias-alvarez/nvim-lsp-ts-utils",
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+  },
 }
+-- generic LSP settings
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.automatic_servers_installation = false
+
+-- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
+-- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
+-- vim.list_extend(lvim.lsp.override, { "pyright" })
+
+-- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pylsp", opts)
+
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+
+-- -- set additional linters
+-- lvim.lang.typescript.formatters = { { exe = "eslint_d"}, { exe = "prettier" } }
+-- lvim.lang.typescriptreact.formatters = lvim.lang.typescript.formatters
+-- lvim.lang.typescript.linters = { { exe = "eslint" } }
+
+-- vim.list_extend(lvim.lsp.override, {
+--    "emmet_ls", "tsserver"
+-- })
+
