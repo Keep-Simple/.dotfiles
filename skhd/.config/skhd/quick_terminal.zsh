0b49fff176dce3ef30f3602c876457f1916cec74
Nikita Yasnogorodskyi <nikita@orca.security>
2022-02-13 10:47:17 +0200

    feat: no animation on quickterminal hotkey

--- skhd/.config/skhd/quick_terminal.zsh
+++ skhd/.config/skhd/quick_terminal.zsh
@@ -2,14 +2,15 @@
 
 local QuickTerminal=$(yabai -m query --windows | jq -r 'map(select(.title=="QuickTerminal")) | .[0]')
 local QuickTerminal_ID=$(echo $QuickTerminal | jq -r '.id')
+local QuickTerminal_PID=$(echo $QuickTerminal | jq -r '.pid')
 
 if [[ $(echo $QuickTerminal) == 'null' ]] 
   then
     open -n /Applications/Alacritty.app --args --title QuickTerminal
   elif [[ $(echo $QuickTerminal | jq -r '."has-focus"') == 'true'  ]]
-  then
-    yabai -m window $QuickTerminal_ID --minimize
-    yabai -m window --focus recent
+    then
+    osascript -e "tell application \"System Events\" to set visible of first process ¬" \
+              -e "whose unix id = ${QuickTerminal_PID} to false"
   else  
     yabai -m window $QuickTerminal_ID --focus
 fi
