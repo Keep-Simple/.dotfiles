48e9e625fd5ad9cc894d4c6823ebb1cc51b2f629
Nikita Yasnogorodskyi <nikita@orca.security>
2022-04-13 21:27:34 +0300

    feat: detect python venv for lsp

--- lvim/.config/lvim/ftplugin/python.lua
+++ lvim/.config/lvim/ftplugin/python.lua
@@ -1,6 +1,31 @@
+local util = require("lspconfig/util")
+local path = util.path
+
+local function get_python_path(workspace)
+	-- Use activated virtualenv.
+	if vim.env.VIRTUAL_ENV then
+		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+	end
+
+	-- Find and use virtualenv in workspace directory.
+	for _, pattern in ipairs({ "*", ".*" }) do
+		local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+		if match ~= "" then
+			return path.join(path.dirname(match), "bin", "python")
+		end
+		match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+		if match ~= "" then
+			local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+			return path.join(venv, "bin", "python")
+		end
+	end
+
+	-- Fallback to system Python.
+	return exepath("python3") or exepath("python") or "python"
+end
+
 local opts = {
 	root_dir = function(fname)
-		local util = require("lspconfig.util")
 		local root_files = {
 			"pyproject.toml",
 			"setup.py",
@@ -15,6 +40,9 @@ local opts = {
 			or util.path.dirname(fname)
 	end,
 	single_file_support = true,
+	before_init = function(_, config)
+		config.settings.python.pythonPath = get_python_path(config.root_dir)
+	end,
 }
 
 local servers = require("nvim-lsp-installer.servers")
