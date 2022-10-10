36b133ee8ca2ff69bd719d1ef950fef69957c606
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2022-10-10 21:42:47 +0200

    feat: smart_gaps now handles window movement

--- yabai/.config/yabai/mac_settings.sh
+++ yabai/.config/yabai/mac_settings.sh
@@ -5,18 +5,17 @@
 osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
-sudo -v
+# sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do
-	sudo -n true
-	sleep 60
-	kill -0 "$$" || exit
-done 2>/dev/null &
+# while true; do
+# 	sudo -n true
+# 	sleep 60
+# 	kill -0 "$$" || exit
+# done 2>/dev/null &
 
 if [[ "$1" != "-d" ]]; then
 	echo "Removing animations, applying settings"
-	sudo nvram SystemAudioVolume=" " # mute startup sound
 	defaults write -g KeyRepeat -int 2
 	defaults write -g InitialKeyRepeat -int 15
 	defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
