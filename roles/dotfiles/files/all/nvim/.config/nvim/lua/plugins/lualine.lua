866357579415fdc265caf03a38cb66ef3e9e5ab8
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2023-07-26 14:08:43 +0200

    chore: cleanup lualine

--- roles/dotfiles/files/all/nvim/.config/nvim/lua/plugins/lualine.lua
+++ roles/dotfiles/files/all/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -68,10 +68,7 @@ return {
 						},
 					},
 				},
-				lualine_y = {
-					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
-					{ "location", padding = { left = 0, right = 1 } },
-				},
+				lualine_y = {},
 			},
 			extensions = { "neo-tree", "lazy" },
 		}
