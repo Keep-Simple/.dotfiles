be64c3338f735ef83481b1b5cfa01630a3a436a4
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2024-11-04 12:04:35 +0100

    feat: yazi add sync/async file run commands

---
+++ roles/dotfiles/files/all/yazi/.config/yazi/plugins/run-file.yazi/init.lua
@@ -0,0 +1,40 @@
+return {
+	entry = function(_, args)
+		if not args[1] then
+			return
+		end
+
+		local block, with_args
+		if args[1] == "1" then
+			block = true
+			with_args = false
+		elseif args[1] == "2" then
+			block = true
+			with_args = true
+		elseif args[1] == "3" then
+			block = false
+			with_args = true
+		end
+
+		if with_args then
+			local cmd_args, event = ya.input({
+				title = block and "Sync run with args:" or "Async run with args:",
+				position = { "hovered", y = 1, w = 50 },
+			})
+
+			if event == 1 then
+				ya.manager_emit("shell", {
+					"$0 " .. cmd_args,
+					block = block,
+					confirm = true,
+				})
+			end
+		else
+			ya.manager_emit("shell", {
+				"$0",
+				block = block,
+				confirm = true,
+			})
+		end
+	end,
+}
